4ad7f6ec5d3e4020246496261e95bd005485df63
Rosenlo <rosenluov@gmail.com>
2021-02-04 19:33:03 +0800

    update treeNode

--- util/util.go
+++ util/util.go
@@ -6,6 +6,8 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+const NULL = -1 << 16
+
 func Ints2TreeNode(ints []int) *TreeNode {
 	n := len(ints)
 	if n == 0 {
@@ -21,15 +23,46 @@ func Ints2TreeNode(ints []int) *TreeNode {
 		node := queue[0]
 		queue = queue[1:]
 
-		node.Left = &TreeNode{Val: ints[i]}
-		queue = append(queue, node.Left)
+		if ints[i] != NULL {
+			node.Left = &TreeNode{Val: ints[i]}
+			queue = append(queue, node.Left)
+		}
+
 		i++
 
-		if i < n {
+		if i < n && ints[i] != NULL {
 			node.Right = &TreeNode{Val: ints[i]}
 			queue = append(queue, node.Right)
 		}
 	}
 
 	return root
+}
+
+func TreeNode2Ints(root *TreeNode) []int {
+	ints := make([]int, 0)
+	if root == nil {
+		return ints
+	}
+
+	queue := []*TreeNode{root}
+
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+
+		if node == nil {
+			if len(queue) > 0 {
+				ints = append(ints, NULL)
+			}
+		} else {
+			ints = append(ints, node.Val)
+			queue = append(queue, node.Left, node.Right)
+		}
+	}
+	n := len(ints)
+	for n > 0 && ints[n-1] == NULL {
+		n--
+	}
+	return ints[:n]
 }
